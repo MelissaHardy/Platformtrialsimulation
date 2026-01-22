9a09bb36cba2f03cd5667a1a90d02a3aabd7d2bf
MelissaHardy <113648082+MelissaHardy@users.noreply.github.com>
2026-01-22 16:34:16 +1000

    Add files via upload

---
+++ config_pt_a.R
@@ -0,0 +1,40 @@
+
+#### Trial Design Parameters ####
+# The type 1 error is 0.025
+# The power is 80%
+# Total sample size was predetermined
+# The total duration required the mean number of patients recruited per site per year
+
+# Total number of patients the platform trial 
+nPatients <- 300 
+
+n_arms <- 5 # Number of treatment arms including BAT
+
+# Total number of patients in each arm - change from 60 to 93 for 2nd study
+patients_per_experimental_arm <- 60
+death0 <- 0.41
+death1 <- 0.36
+death_probs = c(0.41, 0.36, 0.36, 0.36, 0.36)  # mortality rate for the 5 arms
+true_rd <- death1 - death0 # True risk difference
+
+# non-inferiority margin - need to change from 0.20 to 0.15 for second study
+ni_margin <- 0.20  
+death_alt <- death0 + ni_margin  # Experimental treatment is inferior (worse outcome rate)
+mean_sites_per_month <- 10 # mean number of sites activated per month
+max_sites <- 200 # maximimum number of sites for the trial
+mean_patients_per_site_per_year <- 2 # mean number of patients recruited per site per year
+stagger_months <- 6 # Each new trial is staggered by 6 months
+arm_open_months <- c(0, 6, 12, 18)
+arm_open_month <- c(0, 0, stagger_months * 1:(n_arms - 2))
+expected_control_arm <- 60
+
+
+# Inferior death probabilities vector: control remains at 0.41, others worse
+death_probs_inferior <- c(death0, rep(death_alt, n_arms - 1))
+
+nSims <- 10000
+
+# set seed
+set.seed(1234)
+
+##########################################
---
+++ config_trct_a.R
@@ -0,0 +1,25 @@
+
+#### Trial Design Parameters ####
+# The type 1 error is 0.025
+# The power is 80%
+# Total sample size was predetermined
+# The total duration required the mean number of patients recruited per site per year
+
+nPatients <- 120 # number of patients in each RCT
+death0 <- 0.41 # mortality rate for patients receiving 'treatment 0' BAT
+death1 <- 0.36 # mortality rate for patients receiving 'treatment 1' new investigational treatment
+true_rd <- death1 - death0 # True risk difference
+ni_margin <- 0.20 # non-inferiority margin
+death_alt <- death0 + ni_margin  # Experimental treatment is inferior (worse outcome rate)
+mean_sites_per_month <- 10 # mean number of sites activated per month
+max_sites <- 200 # maximum number of sites for the trial
+mean_patients_per_site_per_year <- 2 # mean number of patients recruited per site per year
+nSims <- 10000 # number of trials to simulate
+starts_months <- as.integer(c(0, 6, 12, 18)) #Required for the recruitment simulation
+nTrials <- 4
+site_concurrency_cap <- 2
+
+# set seed
+set.seed(1234)
+
+##########################################
