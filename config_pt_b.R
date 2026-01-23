6df289248044be6a877c41a67deb25ec54a20bd3
MelissaHardy <113648082+MelissaHardy@users.noreply.github.com>
2026-01-23 11:02:59 +1000

    Add files via upload

--- config_pt_b.R
+++ config_pt_b.R
@@ -1,27 +1,26 @@
+#####################################################################
+#### Trial Design Parameters for the platform trials (Scenario B)####
+#####################################################################
 
-#### Trial Design Parameters ####
 # The type 1 error is 0.025
 # The power is 80%
-# Total sample size was predetermined
-# The total duration required the mean number of patients recruited per site per year
+# Sample size was predetermined
 
-# Total number of patients the platform trial - change to from 300 to 465 for 2nd study
+# Total number of patients the platform trial
 nPatients <- 465 
 
-n_arms <- 5 # Number of treatment arms including BAT
+n_arms <- 5 # Number of treatment arms including control arm
 
-# Total number of patients in each arm - change from 60 to 93 for 2nd study
-patients_per_experimental_arm <- 93
-death0 <- 0.41
-death1 <- 0.36
-death_probs = c(0.41, 0.36, 0.36, 0.36, 0.36)  # mortality rate for the 5 arms
+patients_per_experimental_arm <- 93 # Total number of patients in each arm in Scenario B
+death0 <- 0.41 # Mortality rate for the control arm
+death1 <- 0.36 # Mortality rate for the treatment arms
+death_probs = c(0.41, 0.36, 0.36, 0.36, 0.36)  # Mortality rate for the control and 4 treatment arms
 true_rd <- death1 - death0 # True risk difference
 
-# non-inferiority margin - need to change from 0.20 to 0.15 for second study
-ni_margin <- 0.15  
-death_alt <- death0 + ni_margin  # Experimental treatment is inferior (worse outcome rate)
+ni_margin <- 0.15  # non-inferiority margin fro scenario B
+death_alt <- death0 + ni_margin  # Experimental treatment is inferior (worse outcome rate) for the type 1 error rate
 mean_sites_per_month <- 10 # mean number of sites activated per month
-max_sites <- 200 # maximimum number of sites for the trial
+max_sites <- 200 # maximum number of sites for the trial
 mean_patients_per_site_per_year <- 2 # mean number of patients recruited per site per year
 stagger_months <- 6 # Each new trial is staggered by 6 months
 arm_open_months <- c(0, 6, 12, 18)
@@ -37,4 +36,4 @@ nSims <- 10000
 # set seed
 set.seed(1234)
 
-##########################################
+#####################################################################
